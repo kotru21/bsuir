947bfd78e3f55196ffc5e2a717dc4c66b9c35ea0
kotru21 <arsenijbaturo2000@gmail.com>
2025-11-18 14:11:24 +0300

    Update SubmissionsPage.tsx

--- admin/web/src/pages/SubmissionsPage.tsx
+++ admin/web/src/pages/SubmissionsPage.tsx
@@ -93,7 +93,7 @@ export function SubmissionsPage(): ReactElement {
         summaryFullText: fullText,
       };
     });
-  }, [data.items]);
+  }, [data]);
 
   if (isLoading && !data) {
     return <FullscreenSpinner message="Загружаем ответы..." />;
